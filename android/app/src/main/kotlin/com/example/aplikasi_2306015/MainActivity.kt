de9f006feab5daec62748b25302f031f12ad1160
A1shaa <2306015@itg.ac.id>
2026-02-27 20:40:08 +0700

    Commit pertama membuat aplikasi pertama

---
+++ android/app/src/main/kotlin/com/example/aplikasi_2306015/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.aplikasi_2306015
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    String title = 'Aplikasi Pertamaku';
+    String name = 'Aisha Kamil Agustina';
+    String message = 'Halo nama saya $name, selamat datang di $title';
+
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.lightBlue,
+          title: Text(title),
+        ),
+        body: Column(
+          // mengatur posisi content secara vertical
+          mainAxisAlignment: .start,
+          // mengatur posisi secara horizontal
+          crossAxisAlignment: .start,
+          children: [
+            Text(message),
+            Text(message),
+            Text(message),
+            Text(message),
+            Text(message),
+            Text(message),
+          ],
+        ),
+      ),
+    );
+  }
+}
